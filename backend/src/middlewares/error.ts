28b55cbc0728e9ec0c6fd54184440872615e2218
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-07-01 12:52:10 +0800

    fix: status code not defaulting to 500

--- backend/src/middlewares/error.ts
+++ backend/src/middlewares/error.ts
@@ -6,8 +6,11 @@ const errorHandler: ErrorRequestHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  // Default status code to 500 if not provided
-  let statusCode: number = res.statusCode ? res.statusCode : 500;
+  // If statusCode is not set or is a success code, default to 500
+  let statusCode: number = res.statusCode;
+  if (statusCode < 400) {
+    statusCode = 500;
+  }
 
   res.status(statusCode).json({
     message: err.message,
